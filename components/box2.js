ad212c78b7394432e8d17364224e7a3ec91ffd6d
ozkancondek <ozkncndek@gmail.com>
2022-05-16 19:16:32 +0200

    styled system

---
+++ components/box2.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { space, layout, color } from "styled-system";
+const Box = styled("div")(
+  {
+    boxSizing: "border-box",
+  },
+  space,
+  layout,
+  color
+);
